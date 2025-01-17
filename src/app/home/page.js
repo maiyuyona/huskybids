d31a6e7be95e0d382488cafc6f8ff773ff0ed7d5
maiyuyona <166585029+maiyuyona@users.noreply.github.com>
2025-01-16 19:26:35 -0800

    button demo

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,15 @@
-import React from 'react'
-
+"use client"
 const page = () => {
+    function handleClick(event) {
+        event.preventDefault()
+        console.log("button has been clicked")
+    }
+
   return (
-    <div>Hello!</div>
+    <div className='min-h-screen w-full flex flex-col items-center gap-16 '>
+        <button className=' bg-blue-500 rounded p-4 text-white' onClick={handleClick}>Login</button>
+        <button>Sign out</button>
+    </div>
   )
 }
 
