80b2b1f1ccfc40558ada925984188a7bc47174e2
yunsophie <yunsophieli@gmail.com>
2025-01-23 19:19:55 -0800

    Co-authored-by: maiyuyona <maiyuyona@users.noreply.github.com> Co-authored-by: Yonie <Isaiahriveraa@users.noreply.github.com>

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -10,11 +10,20 @@ const page = () => {
         console.log("button has been clicked")
     }
 
+    function newAccount(event) {
+        event.preventDefault()
+        console.log("new account button")
+    }
+
   return (
-    <div className='min-h-screen w-full flex flex-col items-center gap-16 bg-purple-400'>
-        <button className=' bg-blue-500 rounded p-4 text-white' onClick={handleClick}>Login</button>
-        <button>className = 'Sign out'</button>
+    <div>
+      
+      <div className='min-h-screen w-full flex flex-col items-center gap-16 bg-purple-400'>
+        <button className=' bg-blue-500 rounded p-4 text-white ' onClick={handleClick}>Login</button>
+        <button className=' bg-blue-500 rounded p-4 text-white ' onClick={newAccount}>create a new account</button>
+      </div>
     </div>
+    
   )
 }
 
