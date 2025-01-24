5a32f6cb7978692aed15b5c93a1e76c5a070a07f
maiyuyona <166585029+maiyuyona@users.noreply.github.com>
2025-01-23 19:20:04 -0800

    let me commit please

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -12,6 +12,8 @@ const page = () => {
 
   return (
     <div className='min-h-screen w-full flex flex-col items-center gap-16 bg-purple-400'>
+      {/* Logo image */}
+      <img src="/images/logo.png" alt="Logo" className="w-32 h-auto" />
         <button className=' bg-blue-500 rounded p-4 text-white' onClick={handleClick}>Login</button>
         <button>className = 'Sign out'</button>
     </div>
