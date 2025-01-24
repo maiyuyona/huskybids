6fad5cc7850336637e221e92fe6da2d358c0468f
maiyuyona <166585029+maiyuyona@users.noreply.github.com>
2025-01-23 19:43:22 -0800

    changed logo size

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -18,7 +18,7 @@ const page = () => {
   return (
     <div className='min-h-screen w-full flex flex-col items-center gap-16 bg-purple-400'>
       {/* Logo image */}
-      <img src="/images/logo.png" alt="Logo" className="w-32 h-auto" />
+      <img src="/images/logo.png" alt="Logo" className="w-48 h-auto" />
         <button className=' bg-blue-500 rounded p-4 text-white' onClick={handleClick}>Login</button>
         <button className=' bg-blue-500 rounded p-4 text-white ' onClick={newAccount}>create a new account</button>
     </div>
