3dba0ff6eee10d8116e15a742c20a67b8730df37
Elizabeth Y <102310297+elizaxpy@users.noreply.github.com>
2025-05-19 20:38:33 -0700

    Revert "Merge branch 'main' of https://github.com/maiyuyona/huskybids"
    This reverts commit 443b5d4c137646eab6320cd03a4e8b1fb7cc84ad, reversing
    changes made to 76615fcd6e1a18f1326b14f5e752a004e6237fb4.

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { useState } from "react";
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [error, setError] = useState('');
+
+/*
+Use client is used to handle user input and form state.
+useState is a react hook that lets us add state to functional components.
+ */
