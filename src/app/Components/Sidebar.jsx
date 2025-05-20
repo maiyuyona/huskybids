4f6df9bfdb7ee0fdbb295669e0d35d24bd84ee88
Elizabeth Y <102310297+elizaxpy@users.noreply.github.com>
2025-05-19 20:39:01 -0700

    Revert "fixed previous commit"
    This reverts commit 1b6067035e099f78923f103d892b2fe7d3991bf1.

--- src/app/Components/Sidebar.jsx
+++ src/app/Components/Sidebar.jsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import Image from "next/image";
 import BiscuitIcon from "./BiscuitIcon";
 import { usePathname } from 'next/navigation';
+import { useUser } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Sidebar = ({ biscuits = 1000 }) => {
   // Default starting balance of 1000 biscuits
